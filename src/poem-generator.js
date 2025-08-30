f839748a2fffeeea2a0bd5a73055888f0dfae38d
Nourioo <zahra.nouri.sigriswil@gmail.com>
2025-08-30 21:10:27 +0330

    edit

--- src/poem-generator.js
+++ src/poem-generator.js
@@ -1,1 +1,12 @@
-let joj= document.querySelector(h1);
+function generatePoem (event){
+event.preventDefault();
+new Typewriter('#poem', {
+  strings: ["La tombe dit à la rose"],
+  autoStart: true,
+  delay: 1,
+  cursor:"",
+});
+}
+
+let poemFormElement= document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
